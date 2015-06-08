0882a3e30dbcb7023a9c23d1a3cb024b2249bab2
Federico Rampazzo <frampone@gmail.com>
2015-06-08 18:11:35 +0100

    local video for the example

--- example/public/js/app.js
+++ example/public/js/app.js
@@ -363,7 +363,7 @@ module.exports = React.createClass({displayName: "exports",
         React.createElement(Link, {to: "songs"}, "Check out some songs"), " ", React.createElement("br", null), 
         
         React.createElement("video", {autoPlay: true, loop: true}, 
-          React.createElement("source", {src: "https://video-lhr3-1.xx.fbcdn.net/hvideo-xta1/v/t42.3356-2/11301721_10205687863369895_658175490_n.mp4/video-1433283652.mp4.mp4?vabr=349920&oh=c0f12a9469798b90d2be9966ebf95adc&oe=55712E0A&dl=1", type: "video/mp4"})
+          React.createElement("source", {src: "/isomorphic-javascript.mp4", type: "video/mp4"})
         )
       )
     )
